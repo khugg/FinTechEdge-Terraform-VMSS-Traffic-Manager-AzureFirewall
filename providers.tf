2bdd1c02cddc0cec560ece014e3b69c0a2e24cbf
Viviane Huguette <kemshuggs@gmail.com>
2025-01-15 22:48:29 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,9 +15,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id ="ed19ccdf-7b4a-4666-8948-3923639ec896"
-  tenant_id="69a24b27-7e8e-4b16-99b1-99134658842e"
-  client_id="5355660b-7628-43b1-9048-7e52434ce778"
+  subscription_id =""
+  tenant_id=""
+  client_id=""
 
 
  }
